b9ab54a33c4cda533420b27aec91a62d1442d574
林金龍 <garricklin@linjinlongdeMacBook-Pro.local>
2022-12-19 16:51:09 +0800

    restful api hello

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,9 +17,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.mybatis:mybatis:3.5.11")
+	runtimeOnly("org.postgresql:postgresql")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/main/kotlin/com/example/springboot/controller/HelloController.kt
@@ -0,0 +1,12 @@
+package com.example.springboot.controller
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "hello springboot with kotlin"
+    }
+}
