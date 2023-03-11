09777bb7c84cfcdb4515c50ee3c79a03081b1e82
uniquecourage <uniquecourage@gmail.com>
2023-03-11 21:24:04 +0800

    docker run with connecting to local db not in docker container

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,18 @@
 # For Java, try this
-FROM openjdk:17-jdk
+FROM openjdk:17-jdk-slim
+EXPOSE 8080
 # Refer to Gradlew build -> finalName
 ARG JAR_FILE=build/libs/springboot-0.0.1-SNAPSHOT.jar
 # cd App/Spring
 WORKDIR App/Spring
 # cp build/libs/springboot-0.0.1-SNAPSHOT.jar App/Spring/app.jar
 COPY ${JAR_FILE} app.jar
+
+RUN apt-get update && \
+    apt-get install net-tools && \
+    apt-get install iputils-ping -y
+
+
 # java -jar App/Spring/app.jar
-ENTRYPOINT ["java","-jar","app.jar"]
-EXPOSE 8080
+#ENTRYPOINT ["java","-jar","app.jar"]
+CMD java -jar app.jar
--- src/main/kotlin/com/example/springboot/sqlconfig/impl/SqlConfigImpl.kt
+++ src/main/kotlin/com/example/springboot/sqlconfig/impl/SqlConfigImpl.kt
@@ -8,12 +8,22 @@ import org.springframework.stereotype.Service
 class SqlConfigImpl: SqlConfig {
 
     override fun sqlQuery(): Database {
+//        local run
+//        val database = Database.connect(
+//            url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=practice",
+//            driver = "org.postgresql.Driver",
+//            user = "postgres",
+//            password = "123456"
+//        )
+
+        //        docker run and connect to local db
         val database = Database.connect(
-            url = "jdbc:postgresql://localhost:5432/postgres?currentSchema=practice",
+            url = "jdbc:postgresql://host.docker.internal:5432/postgres?currentSchema=practice",
             driver = "org.postgresql.Driver",
             user = "postgres",
             password = "123456"
         )
+
         return database
     }
 }
